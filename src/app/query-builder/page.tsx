b1dd5cb684fd2b2c340807aed3a6a5bd98437c5e
cgolemme <cgolemme@mitre.org>
2024-06-18 10:35:03 -0400

    Reintroduce suspense

--- src/app/query-builder/page.tsx
+++ src/app/query-builder/page.tsx
@@ -1,8 +1,17 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function QueryBuilder() {
+  return (
+    <Suspense>
+      <QueryBuilderContent />
+    </Suspense>
+  );
+}
+
+function QueryBuilderContent() {
   const searchParams = useSearchParams();
   const type = searchParams.get('type');
   const id = searchParams.get('id');
