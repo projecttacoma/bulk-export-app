c25fc08ac306e5f21bf2c2e464a9c3640ff96ef5
cgolemme <cgolemme@mitre.org>
2024-06-24 14:42:17 -0400

    collapseable table

--- src/app/query-builder/page.tsx
+++ src/app/query-builder/page.tsx
@@ -1,8 +1,11 @@
-import { Center, Tabs, TabsList, TabsPanel, TabsTab } from '@mantine/core';
+import { Center, Container, Paper, rem, Tabs, TabsList, TabsPanel, TabsTab, Title, Tooltip } from '@mantine/core';
 import { Suspense } from 'react';
 import classes from './page.module.css';
 import QueryString from '@/components/query-string';
 import TypeParamsPage from '@/components/type-page/type-params-page';
+import { IconInfoCircle } from '@tabler/icons-react';
+
+const parametersHelpText = 'Add parameters to your bulk export query';
 
 export default function QueryBuilder() {
   return (
@@ -12,18 +15,32 @@ export default function QueryBuilder() {
           <QueryString />
         </Suspense>
       </Center>
-      <Tabs defaultValue="type-tab" classNames={classes}>
-        <TabsList grow justify="center">
-          <TabsTab value="type-tab">Type</TabsTab>
-          <TabsTab value="element-tab">Element</TabsTab>
-          <TabsTab value="type-filters-tab">Type Filters</TabsTab>
-        </TabsList>
-        <TabsPanel value="type-tab">
-          <TypeParamsPage />
-        </TabsPanel>
-        <TabsPanel value="element-tab">Element Tab</TabsPanel>
-        <TabsPanel value="type-filters-tab">Type Filters Tab</TabsPanel>
-      </Tabs>
+      <Paper shadow="md" radius="lg" p="sm" withBorder>
+        <Container fluid className={classes.tabContainer} h={rem(450)}>
+          <Title className={classes.selectorTitle}>Parameters</Title>
+          <Tooltip
+            position="right"
+            withArrow
+            transitionProps={{ duration: 200 }}
+            zIndex={400}
+            label={parametersHelpText}
+          >
+            <IconInfoCircle color="gray" />
+          </Tooltip>
+          <Tabs defaultValue="type-tab" classNames={classes} orientation="vertical" radius="md">
+            <TabsList>
+              <TabsTab value="type-tab">Types</TabsTab>
+              <TabsTab value="element-tab">Elements</TabsTab>
+              <TabsTab value="type-filters-tab">Type Filters</TabsTab>
+            </TabsList>
+            <TabsPanel value="type-tab">
+              <TypeParamsPage />
+            </TabsPanel>
+            <TabsPanel value="element-tab">Element Tab coming soon...</TabsPanel>
+            <TabsPanel value="type-filters-tab">Type Filters Tab coming soon...</TabsPanel>
+          </Tabs>
+        </Container>
+      </Paper>
     </>
   );
 }
--- src/components/query-params-table.tsx
+++ src/components/query-params-table.tsx
@@ -9,46 +9,57 @@ import {
   TableTh,
   TableThead,
   TableTr,
-  TableTd
+  TableTd,
+  Collapse
 } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconLayoutBottombarCollapse } from '@tabler/icons-react';
 import { useRecoilState } from 'recoil';
 
 export default function ParamsTable() {
   const [typeParams, setTypeParams] = useRecoilState(activeTypeParamsState);
+  const [opened, { toggle }] = useDisclosure(false);
 
   console.log(typeParams);
 
   return (
-    <TableScrollContainer minWidth={500} type="native" mah={300} mx="auto">
-      <Table highlightOnHover stickyHeader>
-        <TableThead>
-          <TableTr>
-            <TableTh>Filter Type</TableTh>
-            <TableTh>Elements</TableTh>
-            <TableTh>Edit</TableTh>
-            <TableTh>Remove</TableTh>
-          </TableTr>
-        </TableThead>
-        <TableTbody>
-          {typeParams.map(ty => (
-            <TableTr key={ty}>
-              <TableTd>{ty}</TableTd>
-              <TableTd></TableTd>
-              <TableTd></TableTd>
-              <TableTd>
-                <ActionIcon>
-                  <CloseIcon size="sm" onClick={() => setTypeParams(typeParams.filter(filter => ty != filter))} />
-                </ActionIcon>
-              </TableTd>
-            </TableTr>
-          ))}
-        </TableTbody>
-        {typeParams.length < 1 ? (
-          <Table.Caption style={{ padding: '0px 0px 10px 0px' }}>No active parameters.</Table.Caption>
-        ) : (
-          <></>
-        )}
-      </Table>
-    </TableScrollContainer>
+    <>
+      <ActionIcon onClick={toggle} variant="white">
+        <IconLayoutBottombarCollapse />
+      </ActionIcon>
+      <Collapse in={opened}>
+        <TableScrollContainer minWidth={500} type="native" mah={300} mx="auto">
+          <Table highlightOnHover stickyHeader>
+            <TableThead>
+              <TableTr>
+                <TableTh>Filter Type</TableTh>
+                <TableTh>Elements</TableTh>
+                <TableTh>Edit</TableTh>
+                <TableTh>Remove</TableTh>
+              </TableTr>
+            </TableThead>
+            <TableTbody>
+              {typeParams.map(ty => (
+                <TableTr key={ty}>
+                  <TableTd>{ty}</TableTd>
+                  <TableTd></TableTd>
+                  <TableTd></TableTd>
+                  <TableTd>
+                    <ActionIcon>
+                      <CloseIcon size="sm" onClick={() => setTypeParams(typeParams.filter(filter => ty != filter))} />
+                    </ActionIcon>
+                  </TableTd>
+                </TableTr>
+              ))}
+            </TableTbody>
+            {typeParams.length < 1 ? (
+              <Table.Caption style={{ padding: '0px 0px 10px 0px' }}>No active parameters.</Table.Caption>
+            ) : (
+              <></>
+            )}
+          </Table>
+        </TableScrollContainer>
+      </Collapse>
+    </>
   );
 }
