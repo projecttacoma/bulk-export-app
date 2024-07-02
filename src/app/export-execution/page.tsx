1a827eb73f210f0e710aaccc5d9e85a2a35e85c3
cgolemme <cgolemme@mitre.org>
2024-07-02 10:40:49 -0400

    Switched server side fetch to client side fetches.

--- src/app/export-execution/page.tsx
+++ src/app/export-execution/page.tsx
@@ -1,45 +1,39 @@
+'use client';
 import { BuilderRequest, buildExportRequestString } from '@/util/exportRequestBuilders';
 import { Anchor, Flex, Pill, Stack, Title } from '@mantine/core';
-import { notFound } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-export default async function ExecutionPage({ searchParams }: { searchParams: BuilderRequest }) {
-  // if you navigate to page not through app
-  if (Object.keys(searchParams).length === 0) notFound();
+export default function ExecutionPage({ searchParams }: { searchParams: BuilderRequest }) {
+  const [contentLocation, setContentLocation] = useState<string | null>();
+  const [status, setStatus] = useState<number>();
 
-  const kickoffRequestString = buildExportRequestString(searchParams);
-  const kickoffResponse = await executeKickoff(kickoffRequestString);
+  const exportRequestUrl = buildExportRequestString(searchParams);
 
-  const contentLocation = kickoffResponse.headers.get('content-location');
-  const status = kickoffResponse.status;
+  useEffect(() => {
+    fetch(exportRequestUrl).then(response => {
+      setContentLocation(response.headers.get('content-location'));
+      setStatus(response.status);
+    });
+  }, []);
 
   return (
-    <>
-      <Stack>
-        <Flex>
-          <Title order={3} mr="lg">
-            Status:
-          </Title>
+    <Stack>
+      <Flex>
+        <Title order={3} mr="lg">
+          Status:
+        </Title>
+        {status && (
           <Pill size="xl" bg={status === 202 ? 'green' : 'red'} c="white" fw={700}>
             {status}
           </Pill>
-        </Flex>
-        {contentLocation && (
-          <Flex>
-            <Title mr="lg" order={3}>
-              Content Location:
-            </Title>
-            <Anchor href={contentLocation}>{contentLocation}</Anchor>
-          </Flex>
         )}
-      </Stack>
-    </>
+      </Flex>
+      <Flex>
+        <Title mr="lg" order={3}>
+          Content Location:
+        </Title>
+        {contentLocation && <Anchor href={contentLocation}>{contentLocation}</Anchor>}
+      </Flex>
+    </Stack>
   );
-}
-
-async function executeKickoff(exportRequestString: string) {
-  const response = await fetch(exportRequestString).catch(e => {
-    console.log(e);
-    notFound();
-  });
-  return response;
 }
--- src/app/query-selector/page.tsx
+++ src/app/query-selector/page.tsx
@@ -1,23 +1,30 @@
+'use client';
 import ExportType from '@/components/query-selector/export-type';
 import { Grid, rem } from '@mantine/core';
+import { useEffect, useState } from 'react';
 
-export default async function QuerySelector() {
+export default function QuerySelector() {
+  const [dropdownData, setDropdownData] = useState<string[]>();
+  const groupUrl = `${process.env.NEXT_PUBLIC_HOST}:${process.env.NEXT_PUBLIC_PORT}/Group`;
+
+  useEffect(() => {
+    fetch(groupUrl)
+      .then(response => response.json())
+      .then((data: fhir4.Bundle) => {
+        const entries = data.entry;
+        const ids = entries?.map(entry => entry.resource?.id ?? '');
+        setDropdownData(ids);
+      })
+      .catch(error => {
+        console.error('Error: ', error);
+      });
+  }, []);
+
   return (
     <Grid justify="center" align="center" gutter={{ base: 5, xs: 'md', md: 'lg', xl: 50 }} style={{ margin: rem(60) }}>
       <ExportType exportType="patient" />
-      <ExportType exportType="group" dropdownData={await getGroupIds()} />
+      <ExportType exportType="group" dropdownData={dropdownData} />
       <ExportType exportType="system" />
     </Grid>
   );
-}
-
-// Function to get Group Ids from bulk export server
-async function getGroupIds() {
-  const url = `${process.env.NEXT_PUBLIC_HOST}:${process.env.NEXT_PUBLIC_PORT}/Group`;
-  const response = await fetch(url);
-  const resourceBundle: fhir4.Bundle = await response.json();
-  const entries = resourceBundle.entry ?? [];
-  const ids = entries.map(entry => entry.resource?.id ?? '');
-
-  return ids;
 }
