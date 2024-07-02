a9f0b3841d5e24a220b8f3f2745be945614149dd
cgolemme <cgolemme@mitre.org>
2024-07-02 14:11:58 -0400

    Fixed dynamic pages and added kickoff request to query builder page.

--- src/app/export-execution/page.tsx
+++ src/app/export-execution/page.tsx
@@ -1,20 +1,14 @@
 'use client';
-import { BuilderRequest, buildExportRequestString } from '@/util/exportRequestBuilders';
+
 import { Anchor, Flex, Pill, Stack, Title } from '@mantine/core';
-import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 
-export default function ExecutionPage({ searchParams }: { searchParams: BuilderRequest }) {
-  const [contentLocation, setContentLocation] = useState<string | null>();
-  const [status, setStatus] = useState<number>();
+export default function ExecutionPage() {
+  const searchParams = useSearchParams();
 
-  const exportRequestUrl = buildExportRequestString(searchParams);
-
-  useEffect(() => {
-    fetch(exportRequestUrl).then(response => {
-      setContentLocation(response.headers.get('content-location'));
-      setStatus(response.status);
-    });
-  }, []);
+  const encodedContentLocation = searchParams.get('contentLocation');
+  if (!encodedContentLocation) return <Title>Error: Bad content-location</Title>;
+  const contentLocation = decodeURIComponent(encodedContentLocation);
 
   return (
     <Stack>
@@ -22,11 +16,9 @@ export default function ExecutionPage({ searchParams }: { searchParams: BuilderR
         <Title order={3} mr="lg">
           Status:
         </Title>
-        {status && (
-          <Pill size="xl" bg={status === 202 ? 'green' : 'red'} c="white" fw={700}>
-            {status}
-          </Pill>
-        )}
+        <Pill size="xl" bg="green" c="white" fw={700}>
+          202
+        </Pill>
       </Flex>
       <Flex>
         <Title mr="lg" order={3}>
