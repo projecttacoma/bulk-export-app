b6181775f1c978b21fe685ae8f0569f4a19accef
cgolemme <cgolemme@mitre.org>
2024-07-22 15:35:57 -0400

    removed console log

--- data/allSupportedElements.ts
+++ data/allSupportedElements.ts
@@ -2,5 +2,3 @@ import { sortBy, uniq } from 'lodash';
 import { PropertyPaths } from 'fhir-spec-tools';
 
 export const allSupportedElements = sortBy(uniq(Object.values(PropertyPaths.parsedPropertyPaths).flat()));
-
-console.log(allSupportedElements);
